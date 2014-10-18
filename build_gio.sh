9b594b6fca42c31df6262bf2e7710a5b1e96bcdc
Giovanni Mascellani <mascellani@poisson.phc.unipi.it>
2014-10-18 11:22:35 +0200

    Try to use more advanced compiling options.

--- build_gio.sh
+++ build_gio.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+export CFLAGS="-g -O2 -fstack-protector-strong -Wformat -Werror=format-security"
+export CPPFLAGS="-D_FORTIFY_SOURCE=2"
+export CXXFLAGS="-g -O2 -fstack-protector-strong -Wformat -Werror=format-security"
+export FCFLAGS="-g -O2 -fstack-protector-strong"
+export FFLAGS="-g -O2 -fstack-protector-strong"
+export GCJFLAGS="-g -O2 -fstack-protector-strong"
+export LDFLAGS="-Wl,-z,relro"
+export OBJCFLAGS="-g -O2 -fstack-protector-strong -Wformat -Werror=format-security"
+export OBJCXXFLAGS="-g -O2 -fstack-protector-strong -Wformat -Werror=format-security"
+
 (cd src/qt && ./preconfig.sh --qt-config "-no-rpath -system-zlib -system-libjpeg -system-libpng -system-sqlite -plugin-sql-sqlite" && cd ../..)
 src/qt/bin/qmake
 make
